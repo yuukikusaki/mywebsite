5b1e3c192ed5a3b989bbf690100aa054c0739fab
yuukikuskai <2466286500@qq.com>
2019-08-15 15:17:54 +0800

    APP.vue基本布局完成

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # 基础页面 APP.vue
 1. 就一个标题栏
 2. 首页、学习笔记、资料下载、留言板
++ 利用 Element UI 的导航栏
++ 设置 router 
++ :default-active 默认为 当前路由
 
 ## 首页 HomeContainer
 
--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,12 @@
     <el-row>
       <el-col :span="24">
         <div class="grid-content bg-purple-dark">
-          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
-            <el-menu-item index="1">
-              <router-link to="/home">首页</router-link>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <router-link to="/notes">学习笔记</router-link>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <router-link to="/download">资料下载</router-link>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <router-link to="/message">留言板</router-link>
-            </el-menu-item>
+          <el-menu :default-active="this.$route.path" class="el-menu-demo" router mode="horizontal"
+          >
+            <el-menu-item index="/home">首页</el-menu-item>
+            <el-menu-item index="/notes">学习笔记</el-menu-item>
+            <el-menu-item index="/download">资料下载</el-menu-item>
+            <el-menu-item index="/message">留言板</el-menu-item>
           </el-menu>
         </div>
       </el-col>
@@ -34,13 +27,78 @@
 </script>
 
 <style lang="scss" scope>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+ul {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+}
+img {
+  vertical-align: top;
+  border: none;
+}
+
+.clearf:after,
+.clearf:before {
+  content: "";
+  display: block;
+  clear: both;
+}
+
 .top-container {
   width: 100%;
   height: 90px;
   background-color: pink;
 }
-.el-row {
-  height: 40px;
+
+.el-menu--horizontal > .el-menu-item.is-active,
+.el-menu.el-menu--horizontal,
+.el-menu.el-menu--horizontal {
+  border-style: none;
   background-color: #409eff;
+}
+
+// 点击后 失去 焦点 的状态
+.el-menu--horizontal > .el-menu-item.is-active {
+  background-color: rgba(76, 142, 168, 0.5);
+  border-bottom: 2px solid rgb(229, 255, 0);
+  color: #303133;
+}
+// 点击后的 颜色，此时 焦点还在
+.el-menu--horizontal .el-menu-item:not(.is-disabled):focus {
+  background-color: rgba(76, 142, 168, 0.7);
+  border-bottom: 2px solid rgb(229, 255, 0);
+}
+
+// 设置悬浮时的颜色
+.el-menu--horizontal > .el-menu-item:not(.is-disabled):hover {
+  background-color: rgba(101, 101, 179, 0.5);
+}
+.el-menu-demo {
+  background-color: #409eff;
+  padding-left: 80px;
+  .el-menu-item {
+    color: #fff;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    background-color: #409eff;
+  }
 }
 </style>
